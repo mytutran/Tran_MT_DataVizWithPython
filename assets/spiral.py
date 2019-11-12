022bb38e7e9c452dba3637affa0242ed9eb8ca09
Tran <m_tran110573@ats-hr420-dm14.fc.ca>
2019-11-12 14:26:27 -0500

    1

---
+++ assets/spiral.py
@@ -0,0 +1,31 @@
+#importing packages from third parties is the same as local files
+#just import what you need and run the methods
+
+from matplotlib import pyplot
+
+#turte is a drawing library
+from turtle import *
+
+from random import randint
+
+#these re functions that live in the turtle library
+#just reference / use them
+#how "speed" - fast it's gonna draw
+speed(0)
+bgcolor('black')
+
+x = 1
+
+while x < 400:
+	r = randint(0, 255)
+	g = randint(0, 255)
+	b = randint(0, 255)
+
+	colormode(255)
+
+	pencolor(r,g,b)
+
+	fd(200 + x)
+	rt(90.911)
+
+	x = x + 1
